2781b5099e547c6807841e80e131aa635b4107f2
Wouter Potters <wouter@pottersweb.nl>
2020-04-09 16:43:44 +0200

    Added pickle to import of admission.py
    Bugfix

--- covid19_ICU_admission.py
+++ covid19_ICU_admission.py
@@ -9,6 +9,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import pickle
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_predict
